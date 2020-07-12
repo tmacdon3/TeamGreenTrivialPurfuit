5f9a84b869fb5e78a6be29b3d6794a5a3c36851b
Tyler MacDonald <tylercmacdonald@comcast.net>
2020-07-11 20:20:23 -0400

    State enum with base states

---
+++ src/State.py
@@ -0,0 +1,12 @@
+"""
+@author: Tyler MacDonald
+
+Enum for managing the different states
+"""
+
+import enum
+
+class State(enum.Enum):
+    title_screen = 1
+    database_tool = 2
+    gameplay = 3
