d8ff8f4572b0e4bc7f86b0e3f8856a1cb0a37768
Tyler MacDonald <tylercmacdonald@comcast.net>
2020-07-12 00:05:50 -0400

    Title Screen UI Updates

--- src/TitleScreenFrame.py
+++ src/TitleScreenFrame.py
@@ -1,58 +1,38 @@
-"""
-@author: Tyler MacDonald
-
-Color Scheme:
-https://coolors.co/palettes/trending
-Top to Bottom colors
-bg="#e76f51"
-bg="#f4a261"
-bg="#e9c46a"
-bg="#2a9d8f"
-bg="#264653"
-
-"""
-
 import os
 import sys
 import tkinter as tk
 
 LARGE_FONT= ("Verdana", 12)
-    
-x_padding = (25,25)
-y_padding = (25,25)
+
+x_padding = 25
+y_padding = 25
 
 class TitleScreenFrame(tk.Frame):
 
     def __init__(self, master):
         """
         """
-        tk.Frame.__init__(self, master=master)
+        tk.Frame.__init__(self, master=master, bg="#264653")
         ## create first frame with team green icon
-        frm_a = tk.Frame(bg="#264653")
         path_to_logo="resources" + os.path.sep + "team-green-icon.png"
         team_green_logo_image = tk.PhotoImage(file=path_to_logo)
-        lbl_team_green_logo = tk.Label(master=frm_a, image=team_green_logo_image)
+        lbl_team_green_logo = tk.Label(master=self, image=team_green_logo_image)
         lbl_team_green_logo.img = team_green_logo_image
-        lbl_team_green_logo.pack(padx=x_padding, pady=y_padding)
 
         ## create second frame with buttons
-        frm_b = tk.Frame(bg="#264653")
-        btn_new_game = tk.Button(master=frm_b, text="New Game", command=self.new_game_btn_command, bg="#2a9d8f")
-        btn_new_game.pack(padx=x_padding, pady=y_padding)
-        btn_database_tool = tk.Button(master=frm_b, text="Database Tool", command=self.database_tool_btn_command, bg="#2a9d8f")
-        btn_database_tool.pack(padx=x_padding, pady=y_padding)
-        btn_quit_game = tk.Button(master=frm_b, text="Quit Game", command=self.quit_game_btn_command, bg="#2a9d8f")
-        btn_quit_game.pack(padx=x_padding, pady=y_padding)
+        btn_new_game = tk.Button(master=self, text="New Game", command=self.new_game_btn_command, bg="#2a9d8f")
+        btn_database_tool = tk.Button(master=self, text="Database Tool", command=self.database_tool_btn_command, bg="#2a9d8f")
+        btn_quit_game = tk.Button(master=self, text="Quit Game", command=self.quit_game_btn_command, bg="#2a9d8f")
 
         ## create third title frame
-        frm_c = tk.Frame(bg="#264653")
-        lbl_game_title = tk.Label(master=frm_c, font=("Verdana", 44), bg="#2a9d8f", text="Welcome to Trivial Purfuit")
-        lbl_game_title.pack(padx=x_padding, pady=y_padding)
+        lbl_game_title = tk.Label(master=self, font=("Verdana", 44), bg="#2a9d8f", text="Welcome to Trivial Purfuit")
 
-        ## pack frames into the window
-        frm_c.pack(fill=tk.BOTH, side=tk.TOP, expand=True )
-        frm_a.pack(fill=tk.BOTH, side=tk.LEFT, expand=True )
-        frm_b.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True )
+        # layout properly
+        lbl_game_title.grid(row=0, column=0, columnspan=2, padx=x_padding, pady=y_padding)
+        lbl_team_green_logo.grid(row=1, column=0, rowspan=3, padx=x_padding, pady=y_padding)
+        btn_new_game.grid(row=1, column=1, padx=x_padding, pady=y_padding)
+        btn_database_tool.grid(row=2, column=1, padx=x_padding, pady=y_padding)
+        btn_quit_game.grid(row=3, column=1, padx=x_padding, pady=y_padding)
 
     def database_tool_btn_command(self):
         """
