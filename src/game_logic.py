2b87d7b82bbb7a7d94cabccb87724ee90f4d441d
Joseph K. Aulds <jaulds1@jh.edu>
2020-07-11 23:51:11 -0400

    updated Game-Logic class to correctly reflect the current state

--- src/game_logic.py
+++ src/game_logic.py
@@ -10,9 +10,10 @@ class Game_Logic():
                 num_players):
         sm = StateManager()
         sm.transition_state(State.new_game)
+        print("Current state is: {}".format(sm.get_current_state()))
         self.num_players = num_players
         for i in range(num_players):
-            print self.roll_die()
+            print("Player " + str(i+1) + " rolled " + str(self.roll_die()))
 
     def roll_die(self):
         return random.randint(1,6)
@@ -32,11 +33,8 @@ if __name__ == "__main__":
     sm = StateManager()
     gl = Game_Logic(2)
 
-    print gl.roll_die()
 
 
-
-    print("Current state is: {}".format(sm.get_current_state()))
 
     sm.transition_state(State.gameplay)
 
