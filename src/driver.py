f1f148594b3c340bd08edc628f742a315c414081
Mike Macey <42138233+maceman@users.noreply.github.com>
2020-08-02 12:27:08 -0700

    Update driver.py

--- src/driver.py
+++ src/driver.py
@@ -4,9 +4,18 @@ from database import Database
 def main():
     db = Database('localhost', 'root', '')
     db.create_database()
-    db.show_database()
+    # db.show_database()
     db.create_tables()
-    db.display_all_questions()
+    db.insert_data()
+    # db.show_tables()
+    # db.display_all_questions()
+    # data = db.get_all_question_answers()
+    # data = db.get_question_answer_quadruplet('people')
+    # db.change_category_color('people', 'yellow')
+    # db.add_question_answer('sup','supper','people')
+    # db.remove_question_answer('sup', 'people')
+
+
 
 if __name__ == '__main__':
     main()
