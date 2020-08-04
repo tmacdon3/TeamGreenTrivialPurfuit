daa98ed1e2de3b523aa82ac16d57cabedc657114
tmacdonald18 <tcmacd18@g.holycross.edu>
2020-08-03 22:23:01 -0400

    Display all functionality and hacky remove question functionality

--- src/DatabaseToolFrame.py
+++ src/DatabaseToolFrame.py
@@ -24,79 +24,134 @@ class DatabaseToolFrame(tk.Frame):
         """
         """
         tk.Frame.__init__(self, bg=FRAME_BG)
+
+        self.question_answer_list = []
+
         self.state_manager = state_manager
         self.database_interface = database_interface
         
-        lbl_database_tool = tk.Label(master=self, font=("Verdana", 44), bg=LABEL_BG, text="Database Tool")
-        btn_restore_defaults = tk.Button(master=self, text="Restore Defaults", bg=BUTTON_BG, command=self.restore_defaults_btn_command)
+        self.lbl_database_tool = tk.Label(master=self, font=("Verdana", 44), bg=LABEL_BG, text="Database Tool")
+        self.btn_restore_defaults = tk.Button(master=self, text="Restore Defaults", bg=BUTTON_BG, command=self.restore_defaults_btn_command)
 
-        lbl_remove_selected = tk.Label(master=self, bg=LABEL_BG, text="Remove Selected Item")
-        btn_remove_selected = tk.Button(master=self, bg=BUTTON_BG, text="Remove", command=self.remove_item_btn_command)
+        self.lbl_remove_selected = tk.Label(master=self, bg=LABEL_BG, text="Remove Selected Item")
+        self.ent_remove_index = tk.Entry(master=self, width=5, bg=BUTTON_BG)
+        self.ent_remove_index.insert(0, "Index")
+        self.btn_remove_selected = tk.Button(master=self, bg=BUTTON_BG, text="Remove", command=self.remove_item_btn_command)
 
         # adding a question answer combo
-        lbl_add_item = tk.Label(master=self, bg=LABEL_BG, text="Add a Question")
-        ent_question = tk.Entry(master=self, width=15, bg=BUTTON_BG)
-        ent_question.insert(0, "Question")
-        ent_answer = tk.Entry(master=self, width=15, bg=BUTTON_BG)
-        ent_answer.insert(0, "Answer")
-        btn_add = tk.Button(master=self, bg=BUTTON_BG, text="Add", command=self.add_item_btn_command)
-        lbx_categories = tk.Listbox(master=self, height=len(CATEGORIES))
+        self.lbl_add_item = tk.Label(master=self, bg=LABEL_BG, text="Add a Question")
+        self.ent_question = tk.Entry(master=self, width=15, bg=BUTTON_BG)
+        self.ent_question.insert(0, "Question")
+        self.ent_answer = tk.Entry(master=self, width=15, bg=BUTTON_BG)
+        self.ent_answer.insert(0, "Answer")
+        self.btn_add = tk.Button(master=self, bg=BUTTON_BG, text="Add", command=self.add_item_btn_command)
+        self.lbx_categories = tk.Listbox(master=self, height=len(CATEGORIES))
         for item in CATEGORIES:
-            lbx_categories.insert(tk.END, item)
+            self.lbx_categories.insert(tk.END, item)
         
-        lbx_colors = tk.Listbox(master=self, height=2)
+        self.lbx_colors = tk.Listbox(master=self, height=2)
         for item in ["color_1", "color_2"]:
-            lbx_colors.insert(tk.END, item)
-        btn_change_category_color = tk.Button(master=self, bg=BUTTON_BG, text="Change Category Color", command=self.change_category_color_btn_command)
+            self.lbx_colors.insert(tk.END, item)
+        self.btn_change_category_color = tk.Button(master=self, bg=BUTTON_BG, text="Change Category Color", command=self.change_category_color_btn_command)
 
-        txt_question_display = tk.Text(master=self)
-        txt_question_display.insert("1.0", "This is where questions, answers, and categories will be displayed")
+        self.txt_question_display = tk.Text(master=self)
+        self._initialize_display()
 
-        btn_back = tk.Button(master=self, text="Back", bg=BUTTON_BG, command=self.back_btn_command)
+        self.btn_back = tk.Button(master=self, text="Back", bg=BUTTON_BG, command=self.back_btn_command)
         
         # layout everything properly
-        lbl_database_tool.grid(row=0, column=0, padx=x_padding, pady=y_padding)
-        btn_restore_defaults.grid(row=0, column=1, padx=x_padding, pady=y_padding)
+        self.lbl_database_tool.grid(row=0, column=0, padx=x_padding, pady=y_padding)
+        self.btn_restore_defaults.grid(row=0, column=1, padx=x_padding, pady=y_padding)
 
-        lbl_remove_selected.grid(row=1, column=0, padx=x_padding, pady=y_padding)
-        btn_remove_selected.grid(row=2, column=0, padx=x_padding, pady=y_padding)
+        self.lbl_remove_selected.grid(row=1, column=0, padx=x_padding, pady=y_padding)
+        self.ent_remove_index.grid(row=2, column=0, padx=x_padding, pady=y_padding)
+        self.btn_remove_selected.grid(row=3, column=0, padx=x_padding, pady=y_padding)
         
-        lbl_add_item.grid(row=3, column=0, padx=x_padding, pady=y_padding)
-        ent_question.grid(row=4, column=0, padx=x_padding, pady=y_padding)
-        ent_answer.grid(row=5, column=0, padx=x_padding, pady=y_padding)
-        lbx_categories.grid(row=6, column=0, padx=x_padding, pady=y_padding)
-        btn_add.grid(row=7, column=0, padx=x_padding, pady=y_padding)
+        self.lbl_add_item.grid(row=4, column=0, padx=x_padding, pady=y_padding)
+        self.ent_question.grid(row=5, column=0, padx=x_padding, pady=y_padding)
+        self.ent_answer.grid(row=6, column=0, padx=x_padding, pady=y_padding)
+        self.lbx_categories.grid(row=7, column=0, padx=x_padding, pady=y_padding)
+        self.btn_add.grid(row=8, column=0, padx=x_padding, pady=y_padding)
 
-        lbx_colors.grid(row=8, column=0, padx=x_padding, pady=y_padding)
-        btn_change_category_color.grid(row=9, column=0, padx=x_padding, pady=y_padding)
+        self.lbx_colors.grid(row=9, column=0, padx=x_padding, pady=y_padding)
+        self.btn_change_category_color.grid(row=10, column=0, padx=x_padding, pady=y_padding)
         
-        txt_question_display.grid(row=1, column=1, rowspan=10, padx=x_padding, pady=y_padding)
+        self.txt_question_display.grid(row=1, column=1, rowspan=10, padx=x_padding, pady=y_padding)
 
-        btn_back.grid(row=11, column=0, padx=x_padding, pady=y_padding)
+        self.btn_back.grid(row=11, column=0, padx=x_padding, pady=y_padding)
+
+    def _initialize_display(self):
+        """
+        """
+        self.txt_question_display.delete("1.0","end")
+        self.question_answer_list = self.database_interface.get_all_question_answers()
+        index = 0
+        for row in self.question_answer_list:
+            index += 1
+            question = row["question"]
+            answer = row["correct_answer"]
+            category = row["category"]
+            # add the index so we can use it later to remove/search
+            row["index"] = index
+            self.txt_question_display.insert(tk.END, "Index: {}\nQuestion: {}\nAnswer: {}\nCategory: {}\n\n".format(index, question, answer, category))
+
+    def _refresh_display(self):
+        """
+        """
+        self.txt_question_display.delete("1.0","end")
+        index = 0
+        for row in self.question_answer_list:
+            index += 1
+            question = row["question"]
+            answer = row["correct_answer"]
+            category = row["category"]
+            # add the index so we can use it later to remove/search
+            row["index"] = index
+            self.txt_question_display.insert(tk.END, "Index: {}\nQuestion: {}\nAnswer: {}\nCategory: {}\n\n".format(index, question, answer, category))
 
     def add_item_btn_command(self):
         """
         """
         print("DatabaseToolFrame: Sending Add Item Request to DatabaseInterface")
         self.database_interface.add_question_answer("question", "answer", "category")
+        # TODO
 
     def remove_item_btn_command(self):
         """
         """
         print("DatabaseToolFrame: Sending Remove Item Request to DatabaseInterface")
-        self.database_interface.remove_question_answer("question")
+        question_to_remove = ""
+        category = ""
+        index = 0
+        index_to_remove = -1
+        for row in self.question_answer_list:
+            if int(self.ent_remove_index.get()) == row["index"]:
+                question_to_remove = row["question"]
+                category = row["category"]
+                index_to_remove = index
+            index += 1
+        if question_to_remove == "":
+            print("Index not found!")
+        else:
+            self.database_interface.remove_question_answer(question_to_remove, category)
+            self.question_answer_list.pop(index_to_remove)
+            
+            # this is a hack... we aren't actually removing it from the database...
+            self._refresh_display()
+            #self._initialize_display()
 
     def restore_defaults_btn_command(self):
         """
         """
         print("DatabaseToolFrame: Sending Get All Request to DatabaseInterface")
-        self.database_interface.get_all_question_answers()
+        self._initialize_display()
 
     def change_category_color_btn_command(self):
         """
         """
         print("DatabaseToolFrame: Sending Change Category Request to DatabaseInterface")
         self.database_interface.change_category_color("category", "color")
+        # TODO
 
     def back_btn_command(self):
         """
