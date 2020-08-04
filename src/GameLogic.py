8b1e156dc0a98523eb54ee99b6680b969b53e23b
tmacdonald18 <tcmacd18@g.holycross.edu>
2020-08-04 19:56:28 -0400

    Add player_dict to GameLogic

--- src/GameLogic.py
+++ src/GameLogic.py
@@ -24,6 +24,8 @@ class GameLogic():
         self.category_list = []
         for k in self.category_color_dict.keys():
             self.category_list.append(k)
+
+        self.player_dict = {}
         
     # This method generates the matrix for the gameboard 
     def new_game(self):   
@@ -54,9 +56,9 @@ class GameLogic():
             self.order[i] = self.roll_die()
         self.max = self.order.argmax()
 
-        first_player = "Player " + str(self.max+1)
+        first_player = "p" + str(self.max+1)
         print(first_player + " goes first")
-        players = ["Player 1", "Player 2", "Player 3", "Player 4"]
+        players = ["p1", "p2", "p3", "p4"]
 
         for i in range(num_players):
             if first_player == players[i]:
@@ -68,29 +70,22 @@ class GameLogic():
         #initialize player classes for the number of players
         # for i, player in enumerate(self.player_list):
 
-        self.p1 = Player()
-        self.p2 = Player()
-        self.p3 = Player()
-        self.p4 = Player()
+        self.player_dict = {}
+        for i in range(num_players):
+            j = i + 1
+            self.player_dict["p{}".format(j)] = Player()
 
-        self.update_position("p2", (9,19))
-        self.update_position("p1", (5, 15))
-        self.update_score("p1", "ac")
-        self.update_score("p2", "acbd")
-        print(self.get_all_players_positions())
-        print(self.get_all_players_scores())
+        #self.update_position("p2", (9,19))
+        #self.update_position("p1", (5, 15))
+        #self.update_score("p1", "ac")
+        #self.update_score("p2", "acbd")
+        #print(self.get_all_players_positions())
+        #print(self.get_all_players_scores())
 
 
     # Update player position
     def update_position(self, player, new_position):
-        if player == "p1":
-            self.p1.set_position(new_position)
-        elif player == "p2":
-            self.p2.set_position(new_position)
-        elif player == "p3":
-            self.p3.set_position(new_position)
-        elif player == "p4":
-            self.p4.set_position(new_position)
+        self.player_dict[player].set_position(new_position)
 
     """
     Score is updated as follows:
@@ -103,16 +98,8 @@ class GameLogic():
     their score would be "bd"
     """
     def update_score(self, player, new_score):
-        if player == "p1":
-            self.p1.set_score(new_score)
-        elif player == "p2":
-            self.p2.set_score(new_score)
-        elif player == "p3":
-            self.p3.set_score(new_score)
-        elif player == "p4":
-            self.p4.set_score(new_score)     
+        self.player_dict[player].set_score(new_score)   
 
-            
     # Determines which player shall go next.
     def player_turn(self):    
         if (self.player_iterator + 1) >= self.num_players:
@@ -138,12 +125,16 @@ class GameLogic():
         """Use the number index in the matrix to get a color
         """
         return self.category_color_dict[self.category_list[index]]
-    
-    def get_all_players_scores(self):
-        return self.p1.get_score(), self.p2.get_score(), self.p3.get_score(), self.p4.get_score()
 
-    def get_all_players_positions(self):
-        return self.p1.get_position(), self.p2.get_position(), self.p3.get_position(), self.p4.get_position()
+    def get_player_position(self, player):
+        """
+        """
+        return self.player_dict[player].get_position()
+
+    def get_player_score(self, player):
+        """
+        """
+        return self.player_dict[player].get_score()
 
 
 # This class shall be instantiated x number of times, where x is the number of players
